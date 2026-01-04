dbb0e191438f9dbe2d8aed1073d751565ec086e2
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-04 20:15:33 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,14 @@
+#name="Gurkirat singh"
+#for letters in name:
+ #   print(letters)
+
+numbers="123,229,369:889;56789 GURKIRAT singh"
+
+for char in numbers:
+ if char.isupper():
+   print(char)
+#p
+for i in range(0,101,7):
+    print(i)
+for i in range(0,3000,9):
+  print(i)
