63214f7549df8a3af4469bdeacc668e9d740675c
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-04 20:25:56 +0530

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -11,4 +11,12 @@ for char in numbers:
 for i in range(0,101,7):
     print(i)
 for i in range(0,3000,9):
+
   print(i)
+ 
+ for i in range(1,21):
+  for j in range(1,21):
+    print("{0}* {1} is {2}".format(i,j,j*i))
+    
+    
+print('-------------------------- ')
