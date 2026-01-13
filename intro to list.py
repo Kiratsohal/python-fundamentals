346a16258241a35e980bbead92f0cddc91aabc63
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-13 18:42:00 +0530

    Add files via upload

---
+++ intro to list.py
@@ -0,0 +1,11 @@
+computer_devices=["keybord",
+                  "mouse",
+                  "gamepad",
+                  "speakers"
+                  ]
+for devices in computer_devices:
+    print(devices)
+
+print()
+print(computer_devices[0])
+print(computer_devices[0:3])
