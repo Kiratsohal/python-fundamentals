ba8454eb99d2128445e1c5f2aa24657a6da03163
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-13 19:00:30 +0530

    Update intro to list.py

--- intro to list.py
+++ intro to list.py
@@ -9,3 +9,11 @@ for devices in computer_devices:
 print()
 print(computer_devices[0])
 print(computer_devices[0:3])
+
+a=b=c=d=e=computer_devices
+print(a)
+print('adding cream')
+b.append('cream')
+e.append('banana')
+print(c)
+print(d)
