4eb4012740935ff5411f15c5555996e9db9d47db
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-14 19:00:38 +0530

    Add files via upload

---
+++ max min count len.py
@@ -0,0 +1,17 @@
+number1=[1,3,5,700,800]
+mumber2=[0,5,900,800,6,]
+print(max(number1))
+print(min(number1))
+print(len(number1))
+print()
+print(max(mumber2))
+print(min(mumber2))
+print(len(mumber2))
+
+sentence1=['GURKIRAT']
+sentence2=["sohal"]
+a=b=c=d=sentence1
+a.append('singh')
+print(a)
+print(len("gurkirat"))
+print('gurkirat singh'.count("i"))
