e98332b951c0d132bb79784f5f8f0e8c06f097bb
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-16 19:44:44 +0530

    Add files via upload

---
+++ append with index.py
@@ -0,0 +1,27 @@
+available_options=['cpu',
+                   'ram',
+                   'dvd',
+                   'pc',
+                   'ssd']
+current_choice="-"
+computer_parts=[]
+
+while current_choice !="0":
+    if current_choice in"12345":
+        print('adding {}'.format(current_choice))
+        if current_choice=="1":
+            computer_parts.append('cpu')
+        elif current_choice=="2":
+            computer_parts.append('ram')
+        elif current_choice=='3':
+            computer_parts.append('dvd')
+        elif current_choice=='4':
+            computer_parts.append('pc')
+        elif current_choice=='5':
+            computer_parts.append('ssd')
+        
+    else:
+      for parts in available_options:
+          print('{0}: {1}'.format(available_options.index(parts),parts))
+    current_choice=input()
+print(computer_parts)
