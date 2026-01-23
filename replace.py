83178dbf807e0f18a316661f6cc45116757e29f3
Kiratsohal <154003462+Kiratsohal@users.noreply.github.com>
2026-01-23 19:04:35 +0530

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,9 @@
+computer_parts=['computer','cpu','mouse','dvd','ssd']
+print(computer_parts)
+#computer_parts[2]='joystick'
+print(computer_parts)
+#print(computer_parts[3:])
+computer_parts[2:]="hard disk"
+print(computer_parts)
+computer_parts[2:]=["hard disk"]
+print(computer_parts)
